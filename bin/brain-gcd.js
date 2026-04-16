837b08fda473941a11242f89178b0ac5246bff42
Mahabbatik <ezovich0@gmail.com>
2026-04-17 02:22:18 +0300

    7

---
+++ bin/brain-gcd.js
@@ -0,0 +1,4 @@
+#!/usr/bin/env node
+import runGCD from '../src/games/gcd.js';
+
+runGCD();
---
+++ src/games/gcd.js
@@ -0,0 +1,27 @@
+import runEngine from '../index.js';
+
+const description = 'Find the greatest common divisor of given numbers.';
+
+
+const getGCD = (a, b) => {
+  let num1 = a;
+  let num2 = b;
+  while (num2 !== 0) {
+    const temp = num2;
+    num2 = num1 % num2;
+    num1 = temp;
+  }
+  return num1;
+};
+
+const getRoundData = () => {
+  const num1 = Math.floor(Math.random() * 100) + 1; // 
+  const num2 = Math.floor(Math.random() * 100) + 1; //
+  
+  const question = `${num1} ${num2}`;
+  const correctAnswer = String(getGCD(num1, num2));
+  
+  return [question, correctAnswer];
+};
+
+export default () => runEngine(description, getRoundData);
