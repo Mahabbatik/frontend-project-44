470f3974dc05e0bf9ca18fb58b77731eaf287d80
Mahabbatik <ezovich0@gmail.com>
2026-04-17 03:35:17 +0300

    9.1

--- src/games/progression.js
+++ src/games/progression.js
@@ -2,34 +2,27 @@ import runEngine from '../index.js';
 
 const description = 'What number is missing in the progression?';
 
-
-const getRandomInt = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
-
-
-const generateProgression = () => {
- 
-  const length = getRandomInt(5, 10);
-  const start = getRandomInt(1, 50);
-  const step = getRandomInt(1, 10);
+const getRoundData = () => {
+  const length = Math.floor(Math.random() * 6) + 5;
   
+  const start = Math.floor(Math.random() * 20) + 1;
+  
+  const step = Math.floor(Math.random() * 10) + 1;
+  
+  const hiddenIndex = Math.floor(Math.random() * length);
   
   const progression = [];
   for (let i = 0; i < length; i += 1) {
     progression.push(start + i * step);
   }
   
-  
-  const hiddenIndex = getRandomInt(0, length - 1);
   const correctAnswer = String(progression[hiddenIndex]);
   
+  progression[hiddenIndex] = '..';
   
-  const progressionWithGap = [...progression];
-  progressionWithGap[hiddenIndex] = '..';
-  
-
-  const question = progressionWithGap.join(' ');
+  const question = progression.join(' ');
   
   return [question, correctAnswer];
 };
 
-export default () => runEngine(description, generateProgression);
+export default () => runEngine(description, getRoundData);
