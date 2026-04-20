86aff8420b5439c3e086a4dd3472fe2fbf77da03
soulles911 <aswtr.2258@gmail.com>
2026-04-20 11:36:38 +0300

    9.9

--- README.md
+++ README.md
@@ -9,7 +9,7 @@
 
 [![asciicast](https://asciinema.org)](https://asciinema.org/a/0kWBMdKIuNIPfsrn)
 
-[![asciicast](https://asciinema.org)](https://asciinema.org/a/Ku0mvd6mME03pjKB)
+[![asciicast](https://asciinema.org)](https://asciinema.org/a/5EEFZU2g6ILrERgw)
 
 
 [![Quality Gate Status](https://sonarcloud.io/api/project_badges/measure?project=Mahabbatik_frontend-project-44&metric=alert_status)](https://sonarcloud.io/summary/new_code?id=Mahabbatik_frontend-project-44)
--- src/games/calc.js
+++ src/games/calc.js
@@ -3,8 +3,8 @@ import runEngine from '../index.js'
 const description = 'What is the result of the expression?'
 
 const getRoundData = () => {
-  const num1 = Math.floor(Math.random() * 20) + 1
-  const num2 = Math.floor(Math.random() * 10) + 1
+  const num1 = Math.floor(Math.random() * 20) + 1 // eslint-disable-line
+  const num2 = Math.floor(Math.random() * 10) + 1 // eslint-disable-line
   const operators = ['+', '-', '*']
   const operator = operators[Math.floor(Math.random() * operators.length)]
 
